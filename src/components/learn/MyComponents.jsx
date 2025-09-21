7d72ca1eea552356bb204a26618a9a601a147dfd
Nguyễn Đan Huy <Danhuy1219@gmail.com>
2025-09-21 11:29:58 +0700

    video31

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -3,8 +3,17 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = 25;
+    // const hoidanit = true;
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = ['eric', 'hoi dan it'];
+    // const hoidanit = { name: 'eric', age: 25 };
     return (
         <>
+            <div>{console.log("ERIC")}</div>
+            {/* <div>{document.getElementById().style.position = ""}</div> */}
+            <div>{JSON.stringify(hoidanit)}</div>
             <div className="child" style={{ borderRadius: '10px' }}>
                 eric & hoi dan it
             </div>
