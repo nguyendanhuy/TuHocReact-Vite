742e73e1d27349ef63bdcd1b1fe2458c4594517c
Nguyễn Đan Huy <Danhuy1219@gmail.com>
2025-09-21 18:20:45 +0700

    video36

--- src/components/Todo/TodoNew.jsx
+++ src/components/Todo/TodoNew.jsx
@@ -1,9 +1,21 @@
 const TodoNew = (props) => {
     let { addNewToDo } = props;
-    addNewToDo("Danhuy");
+    // addNewToDo("Danhuy");
+    const handleClick = () => {
+        alert("Click me");
+    }
+    const handleOnChange = (name) => {
+        console.log(">>> handle onchange", name);
+
+    }
     return (
         <div className='Todo-new'>
-            <input type='text'></input><button>Add</button>
+            <input type='text'
+                onChange={(event) => { handleOnChange(event.target.value) }}></input>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}>Add
+            </button>
         </div>
     );
 }
