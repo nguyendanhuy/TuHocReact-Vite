c973b9c6e5b29db27f9cbb65020be6df25fd688f
Nguyễn Đan Huy <Danhuy1219@gmail.com>
2025-09-22 15:59:11 +0700

    video50

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,11 +1,14 @@
+import { Link, NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
+        // react không dùng tag a để chuyển hướng mà dùng tag link.
+        //Navlink khac voi link o chỗ tự động thêm class active dựa vào page đã bấm.
         <ul>
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/User">User</a></li>
-            <li><a href="/Product">Product</a></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/User">User</NavLink></li>
+            <li><NavLink to="/Product">Product</NavLink></li>
         </ul>
     )
 }
