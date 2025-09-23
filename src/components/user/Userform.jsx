0d0c12c16adb99aa7068ec072df85f03c58b375b
Nguyễn Đan Huy <Danhuy1219@gmail.com>
2025-09-23 22:30:12 +0700

    video 69

--- src/components/user/Userform.jsx
+++ src/components/user/Userform.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { Input } from 'antd';
 import { Button, Flex } from 'antd';
+import axios from 'axios';
 const UserForm = () => {
     const [fullName, setfullName] = useState("");
     const [email, setEmail] = useState("");
     const [passWord, setPassWord] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState(0);
+    const [phoneNumber, setPhoneNumber] = useState("");
     const handleClick = () => {
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: passWord,
+            phone: phoneNumber,
+        }
         console.log(fullName, email, passWord, phoneNumber);
+        axios.post(URL_BACKEND, data);
     }
     return (
         <div style={{ width: '80%', margin: '20px auto', border: '1px solid #ccc', padding: '20px', borderRadius: '10px', display: "flex", gap: "10px", flexWrap: "wrap", flexDirection: "column" }}>
